752717e6fde131bfef4a7c6c02927eedee7d3ea0
Cauã Diego <tixyelpvd@gmail.com>
2025-12-07 10:58:19 -0300

    fix(vite): disable sourcemap generation for production build

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     minify: true,
-    sourcemap: true,
+    sourcemap: false,
     lib: {
       entry: 'src/index.ts',
       name: '@tixyel/streamelements',
