26eaa8e3de4920a776a8ab6257e8c47b179c75a8
Cauã Diego <tixyelpvd@gmail.com>
2025-12-08 16:10:20 -0300

    feat: implement LOCAL_SE_API with store functionality and initialization method

---
+++ packages/client/src/streamelements/api.ts
@@ -0,0 +1,48 @@
+import { SE_API } from '../types/streamelements/index.js';
+
+const LOCAL_SE_API: SE_API & { store: { list: Record<string, any> } } = {
+  getOverlayStatus: () => {
+    return {
+      isEditorMode: false,
+      muted: false,
+    };
+  },
+  resumeQueue: () => {},
+  responses: {} as Record<string, any>,
+  sendMessage(message: string, data: object) {},
+  counters: {
+    get(key: string): number | null {
+      return null;
+    },
+  },
+  sanitize(message: string): string {
+    return message;
+  },
+  cheerFilter(message: string): string {
+    return message;
+  },
+  setField(key: string, value: string | number | boolean | undefined, reload: boolean) {},
+
+  store: {
+    set: function (name: string, obj: any) {
+      this.list[name] = obj;
+
+      localStorage.setItem('SE_API-STORE', JSON.stringify(LOCAL_SE_API.store.list));
+    },
+    get: async function (name: string) {
+      if (this.list[name]) return this.list[name];
+      else return null;
+    },
+    list: {} as Record<string, any>,
+  },
+};
+
+export function initializeLocalSEAPI() {
+  let lastStore = localStorage.getItem('SE_API-STORE') ?? '';
+
+  let result = lastStore ? JSON.parse(lastStore) : {};
+
+  LOCAL_SE_API.store.list = result;
+
+  return LOCAL_SE_API;
+}
