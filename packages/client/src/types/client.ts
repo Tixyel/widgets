d7f52d1913152eba0285af5d61bccbf20ef3f878
Cauã Diego <tixyelpvd@gmail.com>
2025-12-17 02:45:25 -0300

    refact: Simplify ClientEvents type definition for improved readability

--- packages/client/src/types/client.ts
+++ packages/client/src/types/client.ts
@@ -3,23 +3,8 @@ import { StreamElements } from './streamelements/main.js';
 export type Provider = 'twitch' | 'youtube' | 'kick' | 'facebook' | 'streamelements';
 
 export type ClientEvents =
-  | {
-      provider: 'streamelements';
-      data: StreamElements.Event.Provider.StreamElements.Events;
-    }
-  | {
-      provider: 'twitch';
-      data: StreamElements.Event.Provider.Twitch.Events;
-    }
-  | {
-      provider: 'youtube';
-      data: StreamElements.Event.Provider.YouTube.Events;
-    }
-  | {
-      provider: 'kick';
-      data: StreamElements.Event.Provider.Kick.Events;
-    }
-  | {
-      provider: 'facebook';
-      data: StreamElements.Event.Provider.Facebook.Events;
-    };
+  | { provider: 'streamelements'; data: StreamElements.Event.Provider.StreamElements.Events }
+  | { provider: 'twitch'; data: StreamElements.Event.Provider.Twitch.Events }
+  | { provider: 'youtube'; data: StreamElements.Event.Provider.YouTube.Events }
+  | { provider: 'kick'; data: StreamElements.Event.Provider.Kick.Events }
+  | { provider: 'facebook'; data: StreamElements.Event.Provider.Facebook.Events };
