523dc5332e90cab4bde00899f863a18c78d3f8b1
Cauã Diego <tixyelpvd@gmail.com>
2026-03-18 10:09:33 -0300

    refactor: simplify imports in Data namespace by using Collection directly feat: Add global badges set

--- packages/client/src/data/index.ts
+++ packages/client/src/data/index.ts
@@ -1,33 +1,21 @@
-import {
-  avatars as A,
-  commonBadges as B,
-  css_color_names as CSS,
-  emotes as E,
-  items as I,
-  messages as M,
-  twitch_messages as TM,
-  youtube_messages as YM,
-  normal_messages as NM,
-  names as N,
-  tiers as T,
-  tts as TTS,
-  youtube_emotes as Y,
-} from './collection/index.js';
+import * as Collection from './collection/index.js';
 
 export namespace Data {
-  export const avatars = A;
-  export const badges = B;
-  export const css_color_names = CSS;
-  export const emotes = E;
-  export const items = I;
-  export const names = N;
-  export const tiers = T;
-  export const tts = TTS;
+  export const avatars = Collection.avatars;
+  export const badges = Collection.commonBadges;
+  export const globalBadges = Collection.globalBadges;
 
-  export const twitch_messages = TM;
-  export const youtube_messages = YM;
-  export const normal_messages = NM;
-  export const messages = M;
+  export const css_color_names = Collection.css_color_names;
+  export const emotes = Collection.emotes;
+  export const items = Collection.items;
+  export const names = Collection.names;
+  export const tiers = Collection.tiers;
+  export const tts = Collection.tts;
 
-  export const youtube_emotes = Y;
+  export const twitch_messages = Collection.twitch_messages;
+  export const youtube_messages = Collection.youtube_messages;
+  export const normal_messages = Collection.normal_messages;
+  export const messages = Collection.messages;
+
+  export const youtube_emotes = Collection.youtube_emotes;
 }
