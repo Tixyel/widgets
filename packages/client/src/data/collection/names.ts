06ff6b1c1eec4a8970a3a5ca159ba6670fce08bf
Cauã Diego <tixyelpvd@gmail.com>
2026-03-18 09:19:23 -0300

    feat: update names list with additional entries

--- packages/client/src/data/collection/names.ts
+++ packages/client/src/data/collection/names.ts
@@ -1,11 +1,18 @@
 export const names = [
-  'Local',
   'Tixyel',
+  'awigui',
+  'kurao_w',
   'Urie_s2',
   'itzzcatt',
   'BeniArts',
+  'mynnyori',
+  'DexPixel',
   'Cupidiko',
+  'starkuss_',
+  'catsember',
   'shy_madeit',
+  'SpookySony',
+  'nanagadesign',
 ];
 
 export default names;
