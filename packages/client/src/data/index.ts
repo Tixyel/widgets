2ce926aac0891b9dd427f4e75c4328be7d4bcc27
Cauã Diego <tixyelpvd@gmail.com>
2026-03-18 13:21:12 -0300

    fix: update badges export to use globalBadges instead of commonBadges

--- packages/client/src/data/index.ts
+++ packages/client/src/data/index.ts
@@ -2,8 +2,7 @@ import * as Collection from './collection/index.js';
 
 export namespace Data {
   export const avatars = Collection.avatars;
-  export const badges = Collection.commonBadges;
-  export const globalBadges = Collection.globalBadges;
+  export const badges = Collection.globalBadges;
 
   export const css_color_names = Collection.css_color_names;
   export const emotes = Collection.emotes;
